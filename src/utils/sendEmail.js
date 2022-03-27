44ce6a52367fdb1c97e302527eb68e299b0b9a25
klara <klaravuckov@gmail.com>
2022-03-27 16:04:51 +0200

    Prod url update

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const host = "http://localhost:4000/api/v1/send-email"
+const host = "http://www.maestral-kitesurfing.com:4000/api/v1/send-email"
 
 const sendEmail = (data) => {
     return new Promise((resolve, reject) => {
